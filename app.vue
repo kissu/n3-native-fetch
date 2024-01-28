073e87883592a0d380f0bb6daadd97c764e20594
Konstantin BIFERT <hello@kissu.io>
2024-01-28 14:26:25 +0100

    hm

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <button @click="tryTest">call dat server route</button>
+  <button @click="serverMiddleware">server route middleware</button>
+  <button @click="directFetch">direct fetch</button>
 </template>
 
 <script setup>
@@ -8,5 +10,15 @@ const { data: test, refresh: testRefresh } = await useFetch('/api/netlify-contex
 async function tryTest() {
   await testRefresh()
   console.log('test function', test)
+}
+
+async function serverMiddleware() {
+  const res = await $fetch('/api/netlify-context')
+  console.log('api netlify', res)
+}
+
+async function directFetch() {
+  const res = await $fetch('/netlify-edge')
+  console.log('direct netlify edge', res)
 }
 </script>
